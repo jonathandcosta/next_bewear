ab3ab32c180b81605ab24781d3a110ac8bcdc575
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-12-19 15:36:52 -0300

    feat: show banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 import Header from "@/components/common/header";
-import { Button } from "@/components/ui/button";
+
+import Image from "next/image";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="space-y-6 px-5">
+        <Image
+          src="/img/banner_01.png"
+          alt="Leve a vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="mt-5 h-auto w-full"
+        />
+
+        <div className="flex gap-3"></div>
+
+        <Image
+          src="/img/banner_02.png"
+          alt="Leve a vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="mt-5 h-auto w-full"
+        />
+      </div>
     </>
   );
 };
