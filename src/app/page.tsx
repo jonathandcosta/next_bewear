dc1058209f33da0f86d6b4b372038835969bf776
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-12-22 21:49:38 -0300

    feat: add category select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategorySelector from "@/components/common/category-sellector";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -10,6 +11,8 @@ const Home = async () => {
       variants: true,
     },
   });
+
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -27,6 +30,10 @@ const Home = async () => {
         </div>
 
         <ProductList title="Mais vendidos" products={products} />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
 
         <div className="flex gap-3"></div>
 
