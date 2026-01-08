ab95775955cfeefcaf800c5ada0bf88f7369b53f
Jonathan Costa <jonathanduartecosta@gmail.com>
2026-01-08 18:25:59 -0300

    feat: add styles page slug category

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -32,10 +32,14 @@ const CategoryPage = async ({ params }: CategoryPageProps) => {
   return (
     <>
       <Header />
-      <h2>{category.name}</h2>
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
+      <div className="space-y-6 px-5">
+        <h2 className="text-xl font-semibold">{category.name}</h2>
+        <div className="grid grid-cols-2 gap-4">
+          {products.map((product) => (
+            <ProductItem key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
