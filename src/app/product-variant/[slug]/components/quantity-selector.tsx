3a981520ed08e8a7bea84a4dabe2964fef938417
Jonathan Costa <jonathanduartecosta@gmail.com>
2026-01-29 11:52:02 -0300

    feat: add increment and decrement quantity on the page product

--- src/app/product-variant/[slug]/components/quantity-selector.tsx
+++ src/app/product-variant/[slug]/components/quantity-selector.tsx
@@ -7,15 +7,23 @@ import { useState } from "react";
 const QuantitySelector = () => {
   const [quantity, setQuantity] = useState(1);
 
+  const handleDecrement = () => {
+    setQuantity((prev) => (prev > 1 ? prev - 1 : prev));
+  };
+
+  const handleIncrement = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
   return (
     <div className="space-y-4">
       <div className="font-medium">Quantidade</div>
       <div className="flex w-[100px] items-center justify-between rounded-lg border">
-        <Button size="icon" variant="ghost">
+        <Button size="icon" variant="ghost" onClick={handleDecrement}>
           <MinusIcon />
         </Button>
         <p>{quantity}</p>
-        <Button size="icon" variant="ghost">
+        <Button size="icon" variant="ghost" onClick={handleIncrement}>
           <PlusIcon />
         </Button>
       </div>
